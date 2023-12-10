311b40c335ab83e1d3ab44b8c5e504c914cf61db
prash <prash.sastra1@gmail.com>
2023-12-10 12:47:16 +0530

    Pagination Angular App Logic completed..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, catchError, map, of, startWith } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, map, of, startWith } from 'rxjs';
 import { ApiResponse } from './interface/api-response';
 import { Page } from './interface/page';
 import { HttpErrorResponse } from '@angular/common/http';
@@ -13,12 +13,17 @@ import { UserService } from './service/user.service';
 export class AppComponent implements OnInit {
   // Define state here
   usersState$: Observable<{ appState: String, appData?: ApiResponse<Page>, error?: HttpErrorResponse }>;
+  responseSubject = new BehaviorSubject<ApiResponse<Page>>(null);
+  private currentPageSubject = new BehaviorSubject<number>(0);
+  currentPage$ = this.currentPageSubject.asObservable();
 
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
     this.usersState$ = this.userService.users$().pipe(
       map((response: ApiResponse<Page>) => {
+        this.responseSubject.next(response);
+        this.currentPageSubject.next(response.data.page.number);
         console.log(response);
         return ({ appState: 'APP_LOADED', appData: response });
       }
@@ -26,5 +31,23 @@ export class AppComponent implements OnInit {
       startWith({ appState: 'APP_LOADING' }),
       catchError((error: HttpErrorResponse) => of({ appState: 'APP_ERROR', error }))
     )
+  }
+
+  goToPage(name?: string, pageNumber:number=0): void {
+    this.usersState$ = this.userService.users$(name, pageNumber).pipe(
+      map((response: ApiResponse<Page>) => {
+        this.responseSubject.next(response);
+        this.currentPageSubject.next(pageNumber);
+        console.log(response);
+        return ({ appState: 'APP_LOADED', appData: response });
+      }
+      ),
+      startWith({ appState: 'APP_LOADED', appData: this.responseSubject.value }),
+      catchError((error: HttpErrorResponse) => of({ appState: 'APP_ERROR', error }))
+    )
+  }
+
+  goToNextOrPreviousPage(direction?: string, name?: string): void {
+    this.goToPage(name, direction === 'forward' ? this.currentPageSubject.value + 1 : this.currentPageSubject.value - 1);
   }
 }
